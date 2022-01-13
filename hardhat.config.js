34403d4aa4dc7cf79ad6f0e1c7b1d192113c40d7
Kalju Jake Nekvasil <kajnekvasil@gmail.com>
2022-01-12 19:45:06 -0500

    connected wallet to react-app

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,11 +3,11 @@ require("@nomiclabs/hardhat-waffle");
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+	const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+	for (const account of accounts) {
+		console.log(account.address);
+	}
 });
 
 // You need to export an object to set up your config
@@ -17,5 +17,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+	solidity: "0.8.4",
+	networks: {
+		rinkeby: {
+			url: "https://eth-rinkeby.alchemyapi.io/v2/ZRjZl6GqIAJExQhGomFILfEQ4MBVLV_J",
+			accounts: [
+				"5f5feb28099822b41fb2dd2ed204da9337f828df1cc34981b9550f1f5c844d1e",
+			],
+		},
+	},
 };
